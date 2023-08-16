53367bc1823aaa23eb9c0ff31c28cf5907400b80
Amit Kumar <amitkumar3@einfochips.com>
2023-08-16 12:46:18 +0530

    updating readme again

--- README.c
+++ README.c
@@ -4,3 +4,7 @@ It is file which conation some lines!
 
 Yo yo Honey Singh, THIS DATA WILL BE ADDED WITH THE HELO OF NEW UPDATES BRANCH
 This is a cause for the troubkeshoot!!!!
+
+This is a new line which will bw added by the master branch
+
+yuo yup
